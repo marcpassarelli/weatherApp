479f20f4b01b2afb1f296127150343eaa7df1a69
Marc Victor <marc.carvalho@inteligenciaweb.com.br>
2020-12-24 17:01:47 -0300

    changed name of component to History

--- weatherApp/src/routes/AppStack/index.js
+++ weatherApp/src/routes/AppStack/index.js
@@ -1,33 +1,33 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import * as React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 
-import Home from '../../screens/Home';
-import DetailedDay from '../../screens/DetailedDay';
-import PastDays from '../../screens/PastDays';
+import Home from '../../screens/Home'
+import DetailedDay from '../../screens/DetailedDay'
+import History from '../../screens/History'
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator()
 
 const AppStack = () => (
   <NavigationContainer>
     <Stack.Navigator>
       <Stack.Screen
-        name="Home"
+        name='Home'
         component={Home}
         options={{ headerShown: false }}
       />
       <Stack.Screen
-        name="DetailedDay"
+        name='DetailedDay'
         component={DetailedDay}
         options={{ headerShown: false }}
       />
       <Stack.Screen
-        name="PastDays"
-        component={PastDays}
+        name='History'
+        component={History}
         options={{ headerShown: false }}
       />
     </Stack.Navigator>
   </NavigationContainer>
-);
+)
 
-export default AppStack;
+export default AppStack
