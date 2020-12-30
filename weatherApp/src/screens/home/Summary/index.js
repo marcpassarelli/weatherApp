77e8d0ce9c8a9ad6ad72b62b14d2f90a7a7ae059
Marc Victor <marc.carvalho@inteligenciaweb.com.br>
2020-12-29 22:21:02 -0300

    API integration with search from TextInput

--- weatherApp/src/screens/home/Summary/index.js
+++ weatherApp/src/screens/home/Summary/index.js
@@ -3,30 +3,30 @@ import { Image, Text, View } from 'react-native'
 
 import styles from './styles'
 
-const Summary = ({ weatherInfo }) => {
+const Summary = ({ weatherInfoCurrent, weatherInfoDay }) => {
   return (
     <View style={styles.container}>
       <View style={styles.leftInfo}>
         <Text style={styles.textCurrentTemperature}>
-          {Math.round(weatherInfo.main.temp)}º
+          {Math.round(weatherInfoCurrent.main.temp)}º
         </Text>
         <Image
           style={{ height: 60, width: 60 }}
           source={{
-            uri: `http://openweathermap.org/img/wn/${weatherInfo.weather[0].icon}.png`,
+            uri: `http://openweathermap.org/img/wn/${weatherInfoCurrent.weather[0].icon}.png`,
           }}
         />
       </View>
       <View style={styles.rightInfo}>
         <Text style={styles.textRightInfo}>
-          {Math.round(weatherInfo.main.temp_max)}º /{' '}
-          {Math.round(weatherInfo.main.temp_min)}º
+          {Math.round(weatherInfoDay.temp.max)}º /{' '}
+          {Math.round(weatherInfoDay.temp.min)}º
         </Text>
         <Text style={styles.textRightInfo}>
-          feels like: {Math.round(weatherInfo.main.feels_like)}º
+          feels like: {Math.round(weatherInfoCurrent.main.feels_like)}º
         </Text>
         <Text style={styles.textRightInfo}>
-          {weatherInfo.weather[0].description}
+          {weatherInfoCurrent.weather[0].description}
         </Text>
       </View>
     </View>
