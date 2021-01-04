41b9be50c8f099cf868843c6ff65dd75b5bab31c
Marc Victor <marc.carvalho@inteligenciaweb.com.br>
2021-01-03 23:50:45 -0300

    only making fetch for history after 6 hours for preventing too many calls on free api

--- weatherApp/src/screens/History/index.js
+++ weatherApp/src/screens/History/index.js
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { SafeAreaView, ScrollView, Text, View } from 'react-native'
-import { getLastPosition } from '../../utils/localStorage'
+import {
+  getLastPositionFromLocalStorage,
+  storeLastHistoryInfoInLocalStorage,
+  getLastHistoryInfoFromLocalStorage,
+  hasPassedSixHours,
+} from '../../utils/localStorage'
 import { API_KEY } from '@env'
 import NextHoursForecast from '../../sharedComponents/NextHoursForecast'
 import { format, fromUnixTime } from 'date-fns'
@@ -9,36 +14,52 @@ import styles from './styles'
 const History = () => {
   const [listOfDays, setListOfDays] = useState([])
   const [loading, setLoading] = useState(true)
-  useEffect(() => {
-    getLastPosition().then(({ lat, lon }) => {
-      let promises = []
-      let days = []
-      //5 calls, 1 for each of the past days
-      for (let i = 1; i < 6; i++) {
-        let timeNow = new Date()
-        timeNow.setDate(timeNow.getDate() - i)
-        const dtForDay = Math.round(timeNow / 1000)
-        promises.push(
-          fetch(
-            `http://api.openweathermap.org/data/2.5/onecall/timemachine?lat=${lat}&lon=${lon}&dt=${dtForDay}&units=metric&appid=${API_KEY}`
-          )
-            .then((res) => res.json())
-            .then((result) => {
-              days.push(result)
-            })
-        )
-      }
 
-      Promise.all(promises).then(() => {
-        //sort by ascending order
-        const sortedListOfDays = days.sort(
-          (a, b) => a.current.dt - b.current.dt
-        )
-        setListOfDays(sortedListOfDays)
-        setLoading(false)
-      })
+  useEffect(() => {
+    //if there history info yet
+    getLastPositionFromLocalStorage().then(({ lat, lon }) => {
+      if (hasPassedSixHours()) {
+        loadWeatherHistoryInfo(lat, lon)
+      } else {
+        getLastHistoryInfoFromLocalStorage(lat, lon)
+          .then((historyDays) => {
+            setListOfDays(historyDays)
+            setLoading(false)
+          })
+          .catch((error) => {
+            loadWeatherHistoryInfo(lat, lon)
+          })
+      }
     })
   }, [])
+
+  const loadWeatherHistoryInfo = (lat, lon) => {
+    let promises = []
+    let days = []
+    //5 calls, 1 for each of the past days
+    for (let i = 1; i < 6; i++) {
+      let dtUtc = new Date()
+      dtUtc.setDate(dtUtc.getDate() - i)
+      const dtUnix = Math.round(dtUtc / 1000)
+      promises.push(
+        fetch(
+          `http://api.openweathermap.org/data/2.5/onecall/timemachine?lat=${lat}&lon=${lon}&dt=${dtUnix}&units=metric&appid=${API_KEY}`
+        )
+          .then((res) => res.json())
+          .then((result) => {
+            days.push(result)
+          })
+      )
+    }
+
+    Promise.all(promises).then(() => {
+      //sort by ascending order
+      const sortedListOfDays = days.sort((a, b) => a.current.dt - b.current.dt)
+      setListOfDays(sortedListOfDays)
+      storeLastHistoryInfoInLocalStorage(sortedListOfDays, lat, lon)
+      setLoading(false)
+    })
+  }
 
   return (
     <SafeAreaView>
--- weatherApp/src/utils/localStorage.js
+++ weatherApp/src/utils/localStorage.js
@@ -1,4 +1,5 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import { differenceInHours, getMilliseconds } from 'date-fns'
 export const storeLastPositionInLocalStorage = async (lat, lon) => {
   try {
     await AsyncStorage.setItem('@latitude', lat)
@@ -18,6 +19,43 @@ export const getLastPositionFromLocalStorage = async () => {
         lon: lon,
       }
     }
+  } catch (e) {
+    // error reading value
+  }
+}
+
+export const storeLastHistoryInfoInLocalStorage = async (
+  daysList,
+  lat,
+  lon
+) => {
+  try {
+    const jsonValue = JSON.stringify(daysList)
+    await AsyncStorage.setItem('@historyDaysList' + lat + lon, jsonValue)
+    const currentDay = getMilliseconds(new Date()).toString()
+    await AsyncStorage.setItem('@hourOfSavedHistory' + lat + lon, currentDay)
+  } catch (e) {
+    // saving error
+  }
+}
+
+export const hasPassedSixHours = async (lat, lon) => {
+  try {
+    const dateMillisecondsLocalStorage = await AsyncStorage.getItem(
+      '@hourOfSavedHistory' + lat + lon
+    )
+    const dateLocalStorage = new Date(dateMillisecondsLocalStorage)
+
+    const currentDate = new Date()
+
+    return differenceInHours(currentDate, dateLocalStorage) > 5 ? true : false
+  } catch (error) {}
+}
+
+export const getLastHistoryInfoFromLocalStorage = async (lat, lon) => {
+  try {
+    const jsonValue = await AsyncStorage.getItem('@historyDaysList' + lat + lon)
+    return jsonValue != null ? JSON.parse(jsonValue) : null
   } catch (e) {
     // error reading value
   }
