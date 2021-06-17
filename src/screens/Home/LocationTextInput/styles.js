ad45a881e78f120a2865302ed89943fa80916c03
Marc Victor Passarelli de Carvalho <mvpc1895@gmail.com>
2021-06-17 11:15:07 -0300

    update api used -incomplete

--- src/screens/Home/LocationTextInput/styles.js
+++ src/screens/Home/LocationTextInput/styles.js
@@ -1,34 +1,34 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
     marginTop: 20,
-    width: '90%',
+    width: "90%",
   },
   textInput: {
-    width: '80%',
-    height: 60,
+    width: "80%",
+    height: 45,
     paddingLeft: 10,
     fontSize: 18,
     borderWidth: 2,
     borderBottomLeftRadius: 8,
     borderTopLeftRadius: 8,
-    borderColor: '#2BBDCC',
+    borderColor: "#2BBDCC",
   },
   containerIcon: {
-    height: 60,
-    width: '18%',
-    alignItems: 'center',
-    justifyContent: 'center',
+    height: 45,
+    width: "15%",
+    alignItems: "center",
+    justifyContent: "center",
     borderWidth: 2,
     borderLeftWidth: 0,
     borderBottomRightRadius: 8,
     borderTopRightRadius: 8,
-    borderColor: '#2BBDCC',
+    borderColor: "#2BBDCC",
   },
-})
+});
 
-export default styles
+export default styles;
